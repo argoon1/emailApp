2c62efd6b0846b4a02463f3f971d214f335f9e8a
Kacper Zacharz <xxygrek@wp.pl>
2022-08-05 19:05:20 +0200

    deploy test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,9 +3,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 function App() {
   return (
     <Router>
-      <SocketContext>
-        <></>
-      </SocketContext>
+      <></>
     </Router>
   );
 }
