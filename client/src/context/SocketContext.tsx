661a23f662f0c705ad90f20cbfbc9f1ff327e082
Kacper Zacharz <xxygrek@wp.pl>
2022-08-05 22:06:30 +0200

    test

--- client/src/context/SocketContext.tsx
+++ client/src/context/SocketContext.tsx
@@ -5,55 +5,51 @@ import {
   MessageDataForm,
   UserData,
 } from "../sharedTypes/sendMessageSharedTypes";
-interface SocketContextValue {
-  addNewUser: (name: string) => void;
-  userData: null | UserData;
-  sendNewMessage: (messageData: MessageDataForm, author: string) => void;
-  usersNames: string[];
-}
+// interface SocketContextValue {
+//   addNewUser: (name: string) => void;
+//   userData: null | UserData;
+//   sendNewMessage: (messageData: MessageDataForm, author: string) => void;
+//   usersNames: string[];
+// }
 const socket = io("https://task5-live.herokuapp.com/", {
   transports: ["websocket", "polling", "flashsocket"],
 });
 
-export const SocketContext = createContext<SocketContextValue>({
-  addNewUser: () => {},
-  userData: null,
-  sendNewMessage: () => {},
-  usersNames: [],
-});
+export const SocketContext = createContext({});
 type SocketProviderProps = {
   children: JSX.Element;
 };
 export function SocketProvider({ children }: SocketProviderProps) {
-  const [userData, setUserData] = useState<null | UserData>(null);
-  const [usersNames, setUsersNames] = useState<string[]>([]);
-  const navigate = useNavigate();
-  function addNewUser(name: string) {
-    socket.emit("newUserCreate", name);
-  }
-  function addUserInfoListener() {
-    socket.on("sendUser", ({ user, usersNames }) => {
-      localStorage.setItem("user", JSON.stringify(user));
-      setUserData(user);
-      setUsersNames(usersNames);
-      navigate("/sendmessage");
-    });
-  }
-  function sendNewMessage(messageData: MessageDataForm, author: string) {
-    socket.emit("newMessage", { message: messageData, author });
-  }
-  useEffect(() => {
-    addUserInfoListener();
-  }, []);
-  useEffect(() => {
-    if (!userData) {
-      navigate("/");
-    }
-  }, [userData, navigate]);
-  socket.on("connect", () => {});
+  // const [userData, setUserData] = useState<null | UserData>(null);
+  // const [usersNames, setUsersNames] = useState<string[]>([]);
+  // const navigate = useNavigate();
+  // function addNewUser(name: string) {
+  //   socket.emit("newUserCreate", name);
+  // }
+  // function addUserInfoListener() {
+  //   socket.on("sendUser", ({ user, usersNames }) => {
+  //     localStorage.setItem("user", JSON.stringify(user));
+  //     setUserData(user);
+  //     setUsersNames(usersNames);
+  //     navigate("/sendmessage");
+  //   });
+  // }
+  // function sendNewMessage(messageData: MessageDataForm, author: string) {
+  //   socket.emit("newMessage", { message: messageData, author });
+  // }
+  // useEffect(() => {
+  //   addUserInfoListener();
+  // }, []);
+  // useEffect(() => {
+  //   if (!userData) {
+  //     navigate("/");
+  //   }
+  // }, [userData, navigate]);
+  // socket.on("connect", () => {});
   return (
     <SocketContext.Provider
-      value={{ addNewUser, userData, sendNewMessage, usersNames }}
+      value={{}}
+      //{ addNewUser, userData, sendNewMessage, usersNames }
     >
       {children}
     </SocketContext.Provider>
