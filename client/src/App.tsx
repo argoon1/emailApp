93177d3e829cfdecae4ed194d7e25f324f65fae7
Kacper Zacharz <xxygrek@wp.pl>
2022-08-05 20:49:11 +0200

    socket test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,12 @@
+import { useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { io } from "socket.io-client";
 const socket = io("/api");
 function App() {
-  console.log(socket, "socket test");
+  useEffect(() => {
+    console.log("test");
+    socket.on("connect", () => alert("connected"));
+  }, []);
   return (
     <Router>
       <></>
