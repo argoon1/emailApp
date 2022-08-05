740ca15e41648ac9b537b19d39643dbe7018c5ab
Kacper Zacharz <xxygrek@wp.pl>
2022-08-05 19:23:49 +0200

    test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,8 @@
 import { BrowserRouter as Router } from "react-router-dom";
+import { io } from "socket.io-client";
 function App() {
+  const socket = io("https://task5-live.herokuapp.com/");
+  console.log(socket, "socket test");
   return (
     <Router>
       <></>
