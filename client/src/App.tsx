9840dcfaaf723089c05ccccf71711389cea94433
Kacper Zacharz <xxygrek@wp.pl>
2022-08-05 20:53:32 +0200

    test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import { useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { io } from "socket.io-client";
-const socket = io("/api");
+const socket = io("https://task5-live.herokuapp.com");
 function App() {
   useEffect(() => {
     console.log("test");
+    console.log(socket);
     socket.on("connect", () => alert("connected"));
   }, []);
   return (
